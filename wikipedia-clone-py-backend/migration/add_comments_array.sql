2a10aff23db7bf7a3cb81221a1d531ff182ef700
MisterWayne <baidyfall319@gmail.com>
2025-09-26 00:48:48 +0200

    yes

--- wikipedia-clone-py-backend/migration/add_comments_array.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE revision 
-ALTER COLUMN comments TYPE text[] USING ARRAY[comments],
-ALTER COLUMN comments SET DEFAULT '{}';
-
--- Update existing rows to have empty array if null
-UPDATE revision 
-SET comments = '{}' 
-WHERE comments IS NULL;
